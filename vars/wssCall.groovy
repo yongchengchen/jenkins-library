67658d95ea2b86398d65d980069255b84492c2b2
EC2 Default User <ec2-user@ip-172-31-25-118.ap-southeast-2.compute.internal>
2022-08-17 04:41:02 +0000

    update to support folder

--- vars/wssCall.groovy
+++ vars/wssCall.groovy
@@ -1,8 +1,12 @@
-def call(String api, String command, String notifyTo = "all"){
+def call(String api, String command, String notifyTo = "all", String abfolder=""){
     command = java.net.URLEncoder.encode(command, "UTF-8")
     String secret = env.WSS_SECRET
+    String folderQuery = "";
+    if (abfolder?.trim()) {
+        folderQuery="abfolder=${abfolder}\\&";
+    }
     
-    excuteCode = sh(script: "~/wscat -c ${api}?command=${command}\\&secret=${secret} -e _COMMAND_DONE_ -r 1", returnStatus: true)
+    excuteCode = sh(script: "~/wscat -c ${api}?${folderQuery}command=${command}\\&secret=${secret} -e _COMMAND_DONE_ -r 1", returnStatus: true)
     if (excuteCode == 2) {
         slackNotifyAlert("${excuteCode}", notifyTo)
         error "Job fail"
