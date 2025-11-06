5df7e608cb053dfcab0734ba455b3429267268fd
Yongcheng CHEN <yongcheng.chen@live.com>
2025-11-07 10:47:31 +1100

    Update slackNotifyAlert.groovy

--- vars/slackNotifyAlert.groovy
+++ vars/slackNotifyAlert.groovy
@@ -2,8 +2,17 @@ def call(String msg, String notifyTo) {
     if (notifyTo == "all") {
         notifyTo = "<!here>"
     }
+
     String hook = env.SLACK_NOTIFY_ENDPOINT
     String jobName = env.JOB_NAME
-    msg = "Job:${jobName} Fail"
-    sh("curl -X POST -H 'Content-type: application/json' --data '{\"text\":\"${notifyTo} ${msg}\"}' ${hook}");
+    String buildNumber = env.BUILD_NUMBER
+    String buildUrl = env.BUILD_URL
+    String consoleUrl = "${buildUrl}console"
+
+    msg = "Job: *${jobName}* failed. <${consoleUrl}|View Console Log>"
+
+    sh """curl -X POST -H 'Content-type: application/json' \
+        --data '{"text": "${notifyTo} ${msg}"}' \
+        ${hook}
+    """
 }
